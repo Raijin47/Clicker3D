fb3dacef3eed83854dc8ef71c8be3afac3a96be8
Raijin47 <tony_crosh@mail.ru>
2024-05-22 21:45:44 +0300

    Health bugfix

--- Assets/_Source/Scripts/Health.cs
+++ Assets/_Source/Scripts/Health.cs
@@ -14,19 +14,23 @@ public class Health : MonoBehaviour
     private double _currentHealth;
     private double _maxHealth;
     private string _textMaxHealth;
+    private readonly string _division = " / ";
 
-    private Coroutine _updateMaxHealthCoroutine;
+    private Coroutine _maxHealthUpdateProcessCoroutine;
 
 
     public void Init()
     {
         UpdateMaxHealth();
-        _textMaxHealth = ConvertNumber.Convert(_maxHealth);
         CurrentHealth = YandexGame.savesData.CurrentHealth;
-        GlobalEvent.OnHealthReduction.AddListener(HealthReduction);
+        GlobalEvent.OnHealthReduction.AddListener(UpdateMaxHealth);
 
-        //_updateMaxHealthCoroutine = StartCoroutine(UpdateMaxHealthProcess());
-
+        if(_maxHealthUpdateProcessCoroutine != null)
+        {
+            StopCoroutine(_maxHealthUpdateProcessCoroutine);
+            _maxHealthUpdateProcessCoroutine = null;
+        }
+        _maxHealthUpdateProcessCoroutine = StartCoroutine(MaxHealthUpdateProcess());
     }
 
     public double CurrentHealth
@@ -35,63 +39,60 @@ public class Health : MonoBehaviour
         set
         {
             _currentHealth = value;
-
-            if (_currentHealth >= _maxHealth)
-            {
-                _stage.CurrentStage++;
-                _currentHealth -= _maxHealth;
-                UpdateMaxHealth();
-                return;
-            }
-
-            if (_currentHealth >= _maxHealth)
-            {
-                while (_currentHealth >= _maxHealth)
-                {
-                    _stage.CurrentStage++;
-                    _currentHealth -= _maxHealth;
-                    UpdateMaxHealth();
-                }
-                _textMaxHealth = ConvertNumber.Convert(_maxHealth);
-            }
             UpdateUI();
             YandexGame.savesData.CurrentHealth = _currentHealth;
         }
     }
 
-    private IEnumerator UpdateMaxHealthProcess(double value)
+    private IEnumerator MaxHealthUpdateProcess()
     {
-        while (_currentHealth >= _maxHealth)
+        while (true)
         {
-            _stage.CurrentStage++;
-            _currentHealth -= _maxHealth;
-            UpdateMaxHealth();
+            while (_currentHealth >= _maxHealth)
+            {
+                _stage.CurrentStage++;
+                _currentHealth -= _maxHealth;
+                UpdateMaxHealth();
+                UpdateUI();
+                YandexGame.savesData.CurrentHealth = _currentHealth;
+                yield return null;
+            }
             yield return null;
         }
     }
 
-    private void HealthReduction()
-    {
-        UpdateMaxHealth();
-        _textMaxHealth = ConvertNumber.Convert(_maxHealth);
-    }
-
     private void UpdateUI()
     {
-        _imageHealth.fillAmount = (float)(_currentHealth / _maxHealth);
-        _textHealth.text = ConvertNumber.Convert(_currentHealth) + " / " + _textMaxHealth;
+        if(_currentHealth / _maxHealth > float.MaxValue) _imageHealth.fillAmount = 1;
+        else _imageHealth.fillAmount = (float)(_currentHealth / _maxHealth);
+
+        _textHealth.text = ConvertNumber.Convert(_currentHealth) + _division + _textMaxHealth;
     }
 
     public void OnReset()
     {
         UpdateMaxHealth();
-        _textMaxHealth = ConvertNumber.Convert(_maxHealth);
         CurrentHealth = 0d;
+    }
+
+    private void Start()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 5 == 0)
+            {
+                Debug.Log(i * 10);
+                //Debug.Log("Number " + i + " is divisible by 5");
+                // Здесь можно добавить вашу логику для отметки числа
+            }
+        }
     }
 
     private void UpdateMaxHealth()
     {
-        _maxHealth = IncreaseValue.Calculate(_stage.CurrentStage, _baseHealth, _degreeIncreaseHealth) * Modifier.HealthReductionModifier;
+        _maxHealth = System.Math.Round(System.Math.Pow(_baseHealth, _stage.CurrentStage * 0.01) * Modifier.HealthReductionModifier);
+        //_maxHealth = IncreaseValue.Calculate(_stage.CurrentStage, _baseHealth, _degreeIncreaseHealth) * Modifier.HealthReductionModifier;
+        _textMaxHealth = ConvertNumber.Convert(_maxHealth);
     }
 
 #if UNITY_EDITOR
